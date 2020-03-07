0ee7a5cb1ca85c340dfd46fa5e69ab0c3cfc55ee
yourgentlesmile <iamwinner01@126.com>
2020-03-07 13:46:02 +0800

    LeetCode算法题更新

---
+++ LeetCode/Eleven.java
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode-cn.com/problems/container-with-most-water/
+ */
+public class Eleven {
+    public static int maxArea(int[] height) {
+        int maxArea = -1;
+        int i = 0;
+        int j = height.length - 1;
+        while(i < j) {
+            maxArea = Math.max(Math.min(height[i],height[j]) * (j - i),maxArea);
+            if(height[i] < height[j]) i++;
+            else j--;
+        }
+        return maxArea;
+    }
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+}
---
+++ LeetCode/Twelve.java
@@ -0,0 +1,23 @@
+public class Twelve {
+    public static String intToRoman(int num) {
+        int[] number = new int[]     {1000,900,500,400, 100,90,  50, 40,  10, 9, 5, 4, 1};
+        String[] roman = new String[]{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int i = 0;
+        String res = "";
+        while(num > 0) {
+            int div = num / number[i];
+            if(div != 0) {
+                for(int j = 0;j < div;j++) {
+                    res += (roman[i] + "");
+                }
+            }
+            num = num - number[i] * div;
+            i++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intToRoman(4));
+    }
+}
