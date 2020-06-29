59095f6a5fd133ac20f32a848d5054e349491340
yourgentlesmile <iamwinner01@126.com>
2020-06-29 11:05:46 +0800

    每日一题
    每日一题

--- LeetCode/T215.java
+++ LeetCode/T215.java
@@ -1,40 +1,52 @@
+import java.util.PriorityQueue;
+
 /**
  * 寻找第K大的元素，只需要维护一个大小为K的小顶堆即可
  */
 public class T215 {
     public int findKthLargest(int[] nums, int k) {
-        int[] heap = new int[k];
-        System.arraycopy(nums, 0, heap, 0, heap.length);
-        build(heap);
-        for (int i = k; i < nums.length; i++) {
-            if(heap[0] < nums[i]) {
-                heap[0] = nums[i];
+        build(nums, k);
+        for(int i = k;i < nums.length;i++) {
+            if(nums[0] < nums[i]) {
+                nums[0] = nums[i];
+                heapify(nums,0,k);
             }
-            heapify(heap, 0);
         }
-        return heap[0];
+        return nums[0];
     }
-    public void heapify(int[] heap, int pos) {
-        if(pos >= heap.length) return;
-        int left = pos * 2 + 1;
-        int right = pos * 2 + 2;
-        int min = pos;
-        if(left < heap.length && heap[left] < heap[pos]) {
-            min = left;
+
+    /**
+     * 使用优先队列
+     *
+     */
+    public int secondAnswer(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(k);
+        for(int a : nums) {
+            queue.add(a);
+            if(queue.size() > k) queue.poll();
         }
-        if(right < heap.length && heap[right] < heap[min]) {
-            min = right;
-        }
-        if(min != pos) {
-            int tmp = heap[pos];
-            heap[pos] = heap[min];
-            heap[min] = tmp;
-            heapify(heap, min);
+        return queue.peek();
+    }
+    public void build(int[] arr, int end) {
+        for(int i = (end - 1) / 2;i >= 0;i--) {
+            heapify(arr,i,end);
         }
     }
-    public void build(int[] heap) {
-        for (int i = (heap.length - 1) / 2; i >= 0; i--) {
-            heapify(heap, i);
+    public void heapify(int[] arr, int n, int end) {
+        int l = 2 * n + 1;
+        int r = 2 * n + 2;
+        int min = n;
+        if(l < end && arr[l] < arr[min]) {
+            min = l;
+        }
+        if(r < end && arr[r] < arr[min]) {
+            min = r;
+        }
+        if(min != n) {
+            int t = arr[min];
+            arr[min] = arr[n];
+            arr[n] = t;
+            heapify(arr, min, end);
         }
     }
 }
