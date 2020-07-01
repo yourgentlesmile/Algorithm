6fec4c15bf68ee8bc9165814d50b04cc2a72ff70
yourgentlesmile <iamwinner01@126.com>
2020-07-01 10:54:36 +0800

    每日一题
    每日一题

---
+++ LeetCode/T718.java
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode-cn.com/problems/maximum-length-of-repeated-subarray
+ * 动态规划
+ */
+public class T718 {
+    public int findLength(int[] A, int[] B) {
+        int[][] dp = new int[A.length + 1][B.length + 1];
+        int ans = 0;
+        for (int i = 1; i <= A.length; i++) {
+            for (int j = 1; j <= B.length; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    ans = Math.max(ans, dp[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+}
