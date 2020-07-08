01716e68723a0cf8c74724202b0fe1e9517edc36
yourgentlesmile <iamwinner01@126.com>
2020-07-08 10:03:14 +0800

    每日一题
    每日一题

---
+++ LeetCode/MT1611.java
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode-cn.com/problems/diving-board-lcci
+ */
+public class MT1611 {
+    public int[] divingBoard(int shorter, int longer, int k) {
+        if(k == 0) return new int[0];
+        if(shorter == longer) return new int[]{shorter * k};
+        int[] res = new int[k + 1];
+        for(int i = 0;i <= k;i++) {
+            res[i] = (shorter * (k - i) + longer * i);
+        }
+        return res;
+    }
+}
