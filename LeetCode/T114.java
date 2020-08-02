1f5e56a2b6b519458cf2e14937807554a29caf4d
yourgentlesmile <iamwinner01@126.com>
2020-08-02 21:38:10 +0800

    每日一题

---
+++ LeetCode/T114.java
@@ -0,0 +1,27 @@
+/**
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list
+ *
+ */
+public class T114 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    TreeNode pre = null;
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = pre;
+        root.left = null;
+        pre = root;
+    }
+}
