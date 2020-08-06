bfee18490d8616db17687ccaf21080f4c1e97abb
yourgentlesmile <iamwinner01@126.com>
2020-08-07 07:06:33 +0800

    每日一题

---
+++ LeetCode/T100.java
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode-cn.com/problems/same-tree/
+ */
+public class T100 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null || q == null) {
+            return p == q;
+        }
+        if(p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right,q.right);
+    }
+}
