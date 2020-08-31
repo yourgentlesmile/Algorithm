4d31aee5da9147a9e73c0b142b977ae53a45b6a9
yourgentlesmile <iamwinner01@126.com>
2020-09-01 07:48:25 +0800

    每日一题

---
+++ LeetCode/T486.java
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode-cn.com/problems/predict-the-winner/
+ *
+ * 动态规划与记忆化DFS均可
+ */
+public class T486 {
+    public boolean PredictTheWinner(int[] nums) {
+        int length = nums.length;
+        int[][] dp = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = nums[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][length - 1] >= 0;
+    }
+}
