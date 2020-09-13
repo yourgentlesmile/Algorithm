7afceb90c2698ba3b0ee5d7bf9f8d1a44ee2ec89
yourgentlesmile <iamwinner01@126.com>
2020-09-14 07:52:39 +0800

    每日一题，二叉树非递归中序遍历

---
+++ LeetCode/T94.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+ *
+ *
+ */
+public class T94 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        while(root != null || !stack.isEmpty()) {
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+}
