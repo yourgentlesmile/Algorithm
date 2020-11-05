4d39816db6fe6613b7e84b86c76a480ee9ac1697
yourgentlesmile <iamwinner01@126.com>
2020-11-05 08:30:51 +0800

    每日一题

---
+++ LeetCode/T127.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+/**
+ * https://leetcode-cn.com/problems/word-ladder
+ *
+ * https://leetcode-cn.com/problems/word-ladder/solution/suan-fa-shi-xian-he-you-hua-javashuang-xiang-bfs23/
+ */
+public class T127 {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int end = wordList.indexOf(endWord);
+        if (end == -1) {
+            return 0;
+        }
+        wordList.add(beginWord);
+
+        // 从两端 BFS 遍历要用的队列
+        Queue<String> queue1 = new LinkedList<>();
+        Queue<String> queue2 = new LinkedList<>();
+        // 两端已经遍历过的节点
+        Set<String> visited1 = new HashSet<>();
+        Set<String> visited2 = new HashSet<>();
+        queue1.offer(beginWord);
+        queue2.offer(endWord);
+        visited1.add(beginWord);
+        visited2.add(endWord);
+
+        int count = 0;
+        Set<String> allWordSet = new HashSet<>(wordList);
+
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            count++;
+            if (queue1.size() > queue2.size()) {
+                Queue<String> tmp = queue1;
+                queue1 = queue2;
+                queue2 = tmp;
+                Set<String> t = visited1;
+                visited1 = visited2;
+                visited2 = t;
+            }
+            int size1 = queue1.size();
+            while (size1-- > 0) {
+                String s = queue1.poll();
+                char[] chars = s.toCharArray();
+                for (int j = 0; j < s.length(); ++j) {
+                    // 保存第j位的原始字符
+                    char c0 = chars[j];
+                    for (char c = 'a'; c <= 'z'; ++c) {
+                        chars[j] = c;
+                        String newString = new String(chars);
+                        // 已经访问过了，跳过
+                        if (visited1.contains(newString)) {
+                            continue;
+                        }
+                        // 两端遍历相遇，结束遍历，返回 count
+                        if (visited2.contains(newString)) {
+                            return count + 1;
+                        }
+                        // 如果单词在列表中存在，将其添加到队列，并标记为已访问
+                        if (allWordSet.contains(newString)) {
+                            queue1.offer(newString);
+                            visited1.add(newString);
+                        }
+                    }
+                    // 恢复第j位的原始字符
+                    chars[j] = c0;
+                }
+            }
+        }
+        return 0;
+    }
+}
