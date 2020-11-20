3969bceaa199fc6ebcf8720bca32918b87b78513
yourgentlesmile <iamwinner01@126.com>
2020-11-20 20:19:32 +0800

    每日一题

---
+++ LeetCode/T147.java
@@ -0,0 +1,35 @@
+/**
+ * https://leetcode-cn.com/problems/insertion-sort-list/
+ *
+ * 归并排序
+ */
+public class T147 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head, curr = head.next;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummyHead;
+                while (prev.next.val <= curr.val) {
+                    prev = prev.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead.next;
+    }
+}
